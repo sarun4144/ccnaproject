469fdbb22d59d9a89171152e381837ecbda55054
sarun4144 <sarun4144@gmail.com>
2023-04-26 22:14:36 +0700

    Vercel vertion

--- FinalCCNAFrontend/client/src/Function/Auth.js
+++ FinalCCNAFrontend/client/src/Function/Auth.js
@@ -3,24 +3,24 @@ import axios from 'axios'
 
 
 export const verifycation = async (id) =>
-  await axios.post(process.env.REACT_APP_API + "/verifycation/"+id,{
+  await axios.post(process.env.REACT_APP_API_AUTH + "/verifycation/"+id,{
 });
 export const register = async (value) =>
-  await axios.post(process.env.REACT_APP_API + "/register", value ,{
+  await axios.post(process.env.REACT_APP_API_AUTH + "/register", value ,{
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
 export const userlogin = async (value) =>
-  await axios.post(process.env.REACT_APP_API + "/login", value,{
+  await axios.post(process.env.REACT_APP_API_AUTH + "/login", value,{
     headers: {
         'Content-Type': 'application/json'
     }
 }); 
 
 export const currentuser = async (authtoken) =>
-  await axios.post(process.env.REACT_APP_API + "/current-user", {},
+  await axios.post(process.env.REACT_APP_API_AUTH + "/current-user", {},
   {
     headers:{
       authtoken
@@ -28,7 +28,7 @@ export const currentuser = async (authtoken) =>
 });
 
 export const currentadmin = async (authtoken) =>
-  await axios.post(process.env.REACT_APP_API + "/current-admin", {},
+  await axios.post(process.env.REACT_APP_API_AUTH + "/current-admin", {},
   {
     headers:{
       authtoken
