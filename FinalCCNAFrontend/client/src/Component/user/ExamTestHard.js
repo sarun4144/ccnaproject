2abc28d5dc74424fa6e8fd21d8844d875b1ade7c
sarun4144 <sarun4144@gmail.com>
2023-04-27 20:22:00 +0700

    Image Responsive

--- FinalCCNAFrontend/client/src/Component/user/ExamTestHard.js
+++ FinalCCNAFrontend/client/src/Component/user/ExamTestHard.js
@@ -534,7 +534,7 @@ function ExamTestEasy() {
                                                     </div>
                                                     <center>
                                                         {item.images.map((pic, Ipic) =>
-                                                            <img key={Ipic} src={pic.url} />
+                                                            <img key={Ipic} src={pic.url}  width="100%" height="auto"/>
                                                         )}
                                                     </center>
                                                     <br />
